e6fcf14e13bfb077e2db926f455d51f79664e0bb
SANTHOSHKUMAR S <143958411+Santhoshseeralan@users.noreply.github.com>
2024-04-16 10:07:42 +0530

    private_subnet.tf

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,4 +1,4 @@
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "your_availability_zone"
+  availability_zone = "ap-south-1"
