e1aa394c516ff78ec54414b6783d42f28fc3759b
SANTHOSHKUMAR S <143958411+Santhoshseeralan@users.noreply.github.com>
2024-04-16 10:06:17 +0530

    instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "example" {
-  ami           = "your_ami_id"
+resource "aws_instance" "instance1" {
+  ami           = "ami-0451f2687182e0411"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.instance_sg.name]
