c61f5916b0e6dc6f889198be87a7a09a159a9f9d
SANTHOSHKUMAR S <143958411+Santhoshseeralan@users.noreply.github.com>
2024-04-16 11:12:37 +0530

    instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,12 @@
 resource "aws_instance" "instance1" {
-  ami           = "ami-0451f2687182e0411"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.instance_sg.name]
+  ami                        = "ami-0451f2687182e0411"
+  instance_type              = "t2.micro"
+  subnet_id                  = aws_subnet.public_subnet.id
+  security_groups            = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "instance1"
+  }
 }
+
