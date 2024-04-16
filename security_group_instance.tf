6466b56bc8a3a0c22b44a4e8e83040053a4bbb89
SANTHOSHKUMAR S <143958411+Santhoshseeralan@users.noreply.github.com>
2024-04-16 09:32:23 +0530

    security_group_instance.tf

---
+++ security_group_instance.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "instance_sg" {
+  name        = "instance_sg"
+  description = "Security group for instances"
+
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
